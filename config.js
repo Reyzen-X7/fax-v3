5919522efb29d49d0de386ec1b906f94e398f1f8
Reyzenn 28 <78583226+Reyzen-X7@users.noreply.github.com>
2021-11-03 23:57:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 userbot = {
- owner: [ "6282334170916" ],
+ owner: [ "6283148375193" ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
@@ -11,7 +11,7 @@ userbot = {
    eror: "Sepertinya Eror",
    waits: "Sedang di proses",
    butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU', // buat Image Button
-   packname: "RIXLE-BOT OFFICIAL",
+   packname: "LEYZCODE-BOT OFFICIAL",
    author: 'Arifi Razzaq',
     setting: {
     admin: "only admin",
